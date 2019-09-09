5a7c22970b981f38207dae7b0888511e58abb77c
firnandakusuma <firnandapratama99@gmail.com>
2019-09-09 10:47:19 +0700

    Tebak Angka Final

---
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/SimpleActivity.java
@@ -0,0 +1,28 @@
+package id.ac.polinema.androidlifecycle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimpleActivity extends AppCompatActivity {
+    private EditText nameInput;
+    private TextView outputText;
+
+    public void handleSubmit(View view) {
+            String name = nameInput.getText().toString();
+            outputText.setText("Hello " + name);
+        }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+        nameInput = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
+    }
+}
--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -12,25 +12,37 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-	//  TODO: deklarasikan variabel di sini
+	private int angka;
+	private EditText angkaInput;
+	private Button btn;
+	private int angkaTebakan;
+	Random random = new Random();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// TODO: bind layout di sini
+		intitRandomNumber();
 	}
 
-	// TODO: generate angka random di sini
-	private void initRandomNumber() {
-
-	}
+	private void intitRandomNumber() { angka = random.nextInt(100) + 1; }
 
 	public void handleGuess(View view) {
-		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		angkaInput = findViewById(R.id.number_input);
+		angkaTebakan = Integer.parseInt(String.valueOf(angkaInput.getText()));
+		if (angkaTebakan == angka) {
+			Toast.makeText(this, "Tebakan anda benar!", Toast.LENGTH_SHORT).show();
+			btn = findViewById(R.id.guess_button);
+			btn.setEnabled(false);
+		} else if (angkaTebakan > angka) {
+			Toast.makeText(this, "Tebakan anda terlalu besar!", Toast.LENGTH_SHORT).show();
+		} else if (angkaTebakan < angka) {
+			Toast.makeText(this, "Tebakan anda terlalu kecil!", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	public void handleReset(View view) {
-		// TODO: Reset tampilan
+		setContentView(R.layout.activity_main);
+		intitRandomNumber();
 	}
 }
